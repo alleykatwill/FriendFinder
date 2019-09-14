a8b20d252585210051c8baafa9458c475c83b6e5
alleykatwill <48265377+alleykatwill@users.noreply.github.com>
2019-09-14 13:59:11 -0400

    updates

---
+++ app/data/roommate.js
@@ -0,0 +1,22 @@
+var roommates = [
+  {
+    name: "Benjamin Browder",
+    photo:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    scores: ["5", "1", "4", "3", "1", "5", "1", "4", "3", "1"]
+  },
+  {
+    name: "Clayton Bigsby",
+    photo:
+      "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters_opt/p-Chapelles-Show-Dave-Chapelle1.jpg",
+    scores: ["3", "1", "5", "4", "1", "5", "1", "1", "5", "3"]
+  },
+  {
+    name: "Rubeus Hagrid",
+    photo:
+      "https://i.pinimg.com/originals/e8/48/71/e84871d1be43ce586cf9b7fb26b88d96.jpg",
+    score: ["3", "1", "4", "1", "4", "3", "5", "1", "5", "4"]
+  }
+];
+
+module.exports = roommates;
