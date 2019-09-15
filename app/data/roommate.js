7af895d1d31bc042d4a1343697efbaf6f887d327
alleykatwill <48265377+alleykatwill@users.noreply.github.com>
2019-09-15 18:25:14 -0400

    updates to roomate.js and npm installs

--- app/data/roommate.js
+++ app/data/roommate.js
@@ -1,11 +1,5 @@
 var roommates = [
   {
-    name: "Benjamin Browder",
-    photo:
-      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    scores: ["5", "1", "4", "3", "1", "5", "1", "4", "3", "1"]
-  },
-  {
     name: "Clayton Bigsby",
     photo:
       "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters_opt/p-Chapelles-Show-Dave-Chapelle1.jpg",
@@ -16,7 +10,27 @@ var roommates = [
     photo:
       "https://i.pinimg.com/originals/e8/48/71/e84871d1be43ce586cf9b7fb26b88d96.jpg",
     score: ["3", "1", "4", "1", "4", "3", "5", "1", "5", "4"]
+  },
+  {
+    name: "Jon Snow",
+    photo:
+      "https://vignette.wikia.nocookie.net/gameofthrones/images/d/d0/JonSnow8x06.PNG/revision/latest?cb=20190714094440",
+    score: ["4", "3", "2", "2", "5", "4", "3", "2", "5", "5"]
+  },
+  {
+    name: "Kaladin Stormblessed",
+    photo:
+      "https://66.media.tumblr.com/22996e3c5eed1ce9a6af9519f65ce22e/tumblr_npww12UYbE1ttvln6o1_1280.jpg",
+    score: ["5", "5", "1", "4", "5", "4", "5", "1", "3", "5"]
+  },
+  {
+    name: "Wanda",
+    photo:
+      "https://i.pinimg.com/originals/5e/b7/46/5eb746e88d7b7e789e3929c26acc0124.jpg",
+    score: ["1", "2", "1", "3", "2", "3", "2", "1", "3", "5"]
   }
 ];
 
 module.exports = roommates;
+// roomates is the object. scores is an array of numbers that will be scored and compared
+//  to what the user puts in the form.
--- server.js
+++ server.js
@@ -2,16 +2,21 @@
 
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 // Tells node that we are creating an "express" server.
 var app = express();
 
 // Sets initial port.
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("app/public"));
 
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 
